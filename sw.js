e5641379ff50083129c7515fce9ac43a8d807baf
xx-aa-zzz <37abdalrheem@gmail.com>
2026-07-26 05:35:00 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
-});
